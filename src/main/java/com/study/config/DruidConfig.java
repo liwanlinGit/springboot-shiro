8dd1c769b9d95df56927ca7f82ba37e275f66cb4
519978232 <519978232@qq.com>
2018-01-31 16:18:14 +0800

    【修改sql监控】

--- src/main/java/com/study/config/DruidConfig.java
+++ src/main/java/com/study/config/DruidConfig.java
@@ -1,15 +1,24 @@
 package com.study.config;
 
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+
 @Configuration
 public class DruidConfig {
-
+    @Bean 
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
     @Bean
     public ServletRegistrationBean druidServlet() {
 
